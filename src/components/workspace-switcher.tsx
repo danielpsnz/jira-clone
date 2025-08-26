9c0d5165bd40cf9b4ebdab4a5e3c18d16bb68439
Daniel Pérez <dpdaniperezsanz@gmail.com>
2025-08-26 20:52:33 +0200

    Create workspace members

--- src/components/workspace-switcher.tsx
+++ src/components/workspace-switcher.tsx
@@ -1,8 +1,11 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import { RiAddCircleFill } from "react-icons/ri";
 
 import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
+import { WorkspaceAvatar } from "@/features/workspaces/components/workspace-avatar";
+import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
 
 import {
   Select,
@@ -11,10 +14,15 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { WorkspaceAvatar } from "@/features/workspaces/components/workspace-avatar";
 
 export const WorkspaceSwitcher = () => {
+  const workspaceId = useWorkspaceId();
+  const router = useRouter();
   const { data: workspaces } = useGetWorkspaces();
+
+  const onSelect = (id: string) => {
+    router.push(`/workspaces/${id}`);
+  };
 
   return (
     <div className="flex flex-col gap-y-2">
@@ -22,7 +30,7 @@ export const WorkspaceSwitcher = () => {
         <p className="text-sm uppercase text-neutral-500">Workspaces</p>
         <RiAddCircleFill className="size-5 text-neutral-500 cursor-pointer" />
       </div>
-      <Select>
+      <Select onValueChange={onSelect} value={workspaceId}>
         <SelectTrigger className="w-full bg-neutral-200 font-meidum p-1">
           <SelectValue placeholder="No workspace selected" />
         </SelectTrigger>
@@ -32,7 +40,7 @@ export const WorkspaceSwitcher = () => {
               <div className="flex justify-start items-center gap-3 font-medium">
                 <WorkspaceAvatar
                   name={workspace.name}
-                  image={workspace.image}
+                  image={workspace.imageUrl}
                 />
                 <span className="truncate">{workspace.name}</span>
               </div>
--- src/features/workspaces/schemas.ts
+++ src/features/workspaces/schemas.ts
@@ -1,13 +1,9 @@
 import { z } from "zod";
 
-const fileSchema =
-  typeof File !== "undefined" ? z.instanceof(File) : z.any();
-
 export const createWorkspaceSchema = z.object({
   name: z.string().trim().min(1, "Required"),
-  image: z
-    .union([
-      fileSchema,
+  image: z.union([
+      z.instanceof(File),
       z.string().transform((value) => (value === "" ? undefined : value)),
     ])
     .optional(),
