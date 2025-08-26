260c8a46a5f8956ba7d80e231883cccec80aa618
Daniel Pérez <dpdaniperezsanz@gmail.com>
2025-08-26 12:47:12 +0200

    Workspace switcher

--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -7,49 +7,60 @@ import { sessionMiddleware } from "@/lib/session-middleware";
 
 import { createWorkspaceSchema } from "../schemas";
 
-const app = new Hono().post(
-  "/",
-  zValidator("form", createWorkspaceSchema),
-  sessionMiddleware,
-  async (c) => {
+const app = new Hono()
+  .get("/", sessionMiddleware, async (c) => {
     const databases = c.get("databases");
-    const storage = c.get("storage");
-    const user = c.get("user");
 
-    const { name, image } = c.req.valid("form");
-
-    let uploadedImageUrl: string | undefined;
-
-    if (image instanceof File) {
-      const file = await storage.createFile(
-        IMAGES_BUCKET_ID,
-        ID.unique(),
-        image
-      );
-
-      const arrayBuffer = await storage.getFilePreview(
-        IMAGES_BUCKET_ID,
-        file.$id
-      );
-
-      uploadedImageUrl = `data:image/png;base64,${Buffer.from(
-        arrayBuffer
-      ).toString("base64")}`;
-    }
-
-    const workspace = await databases.createDocument(
+    const workspaces = await databases.listDocuments(
       DATABASES_ID,
-      WORKSPACE_ID,
-      ID.unique(),
-      {
-        name,
-        userId: user.$id,
-        imageUrl: uploadedImageUrl,
-      }
+      WORKSPACE_ID
     );
 
-    return c.json(workspace, 201);
-  }
-);
+    return c.json({ data: workspaces });
+  })
+  .post(
+    "/",
+    zValidator("form", createWorkspaceSchema),
+    sessionMiddleware,
+    async (c) => {
+      const databases = c.get("databases");
+      const storage = c.get("storage");
+      const user = c.get("user");
+
+      const { name, image } = c.req.valid("form");
+
+      let uploadedImageUrl: string | undefined;
+
+      if (image instanceof File) {
+        const file = await storage.createFile(
+          IMAGES_BUCKET_ID,
+          ID.unique(),
+          image
+        );
+
+        const filePreview = await storage.getFilePreview(
+          IMAGES_BUCKET_ID,
+          file.$id
+        );
+
+        uploadedImageUrl = `data:image/png;base64,${Buffer.from(
+          filePreview
+        ).toString("base64")}`;
+      }
+
+      const workspace = await databases.createDocument(
+        DATABASES_ID,
+        WORKSPACE_ID,
+        ID.unique(),
+        {
+          name,
+          userId: user.$id,
+          imageUrl: uploadedImageUrl,
+        }
+      );
+
+      return c.json(workspace, 201);
+    }
+  );
 
 export default app;
