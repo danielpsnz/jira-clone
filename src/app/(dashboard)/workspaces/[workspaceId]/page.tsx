f183bbf66d5d628d86193d2caabc536ff6b603f9
Daniel Pérez <dpdaniperezsanz@gmail.com>
2025-08-28 14:51:27 +0200

    Protect standalone routes

--- src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
@@ -1,7 +1,12 @@
-const WorkspaceIdPage = () => {
-    return (
-        <div>Workspace Id</div>
-    );
-}
+import { redirect } from "next/navigation";
+
+import { getCurrent } from "@/features/auth/actions";
+
+const WorkspaceIdPage = async () => {
+  const user = await getCurrent();
+  if (!user) redirect("/sign-in");
+
+  return <div>Workspace Id</div>;
+};
 
 export default WorkspaceIdPage;
