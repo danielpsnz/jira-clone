ba110aee9255e0a17cd898c2a4df3c9b06b21de1
Daniel Pérez <dpdaniperezsanz@gmail.com>
2025-08-28 14:48:46 +0200

    Build a standalone layout

---
+++ src/app/(standalone)/workspaces/create/page.tsx
@@ -0,0 +1,11 @@
+import { CreateWorkspaceForm } from "@/features/workspaces/components/create-workspace-form";
+
+const WorkspaceCreatePage = () => {
+  return (
+    <div className="w-full lg:max-w-lg">
+      <CreateWorkspaceForm />
+    </div>
+  );
+};
+
+export default WorkspaceCreatePage;
--- src/features/workspaces/components/create-workspace-form.tsx
+++ src/features/workspaces/components/create-workspace-form.tsx
@@ -24,6 +24,7 @@ import { createWorkspaceSchema } from "../schemas";
 import { useCreateWorkspace } from "../api/use-create-workspace";
 import { ImageIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { cn } from "@/lib/utils";
 
 interface CreateWorkspaceFormProps {
   onCancel?: () => void;
@@ -160,6 +161,7 @@ export const CreateWorkspaceForm = ({ onCancel }: CreateWorkspaceFormProps) => {
                 variant="secondary"
                 onClick={onCancel}
                 disabled={isPending}
+                className={cn(!onCancel && "invisible")}
               >
                 Cancel
               </Button>
